1b8ef0b3941321bb723adca4b92a35341753ae99
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-06-07 14:35:50 -0400

    getting some toys !!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const addBtn = document.querySelector('#new-toy-btn')
+const addToyForm = document.querySelector('.add-toy-form')
 const toyForm = document.querySelector('.container')
+
 let addToy = false
+
+
 
 // YOUR CODE HERE
 
@@ -16,4 +20,68 @@ addBtn.addEventListener('click', () => {
 })
 
 
-// OR HERE!
+
+let toyURL = 'http://localhost:3000/toys';
+
+//get function
+const get = (url) => {
+	return fetch(url).then(resp => resp.json())
+}
+
+//post function
+const post = (url, params) => {
+	return fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(params),
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	}).then(resp => resp.json()).catch(err => {
+		console.error(err)
+	})
+}
+
+
+// Dom Content Loaded Event Listener
+document.addEventListener('DOMContentLoaded', () => {
+	// selectors
+	const toyCollection = document.querySelector("#toy-collection");
+
+	// Read all toys - calling above get function
+	get(toyURL).then(toyInfo => {
+		toyInfo.forEach(toy => {
+			toyCollection.innerHTML += `<div class="card">
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+  </div>` 
+		})
+	})
+
+	// Post New Toys
+	addToyForm.addEventListener('submit', (e) => {
+		e.preventDefault()
+		post(toyURL, { 
+			name: e.target.name.value,
+			image: e.target.image.value,
+			likes: 0
+		}).then(newToy => {
+			toyCollection.innerHTML += `<div class="card">
+    		<h2>${newToy.name}</h2>
+    		<img src="${newToy.image}" class="toy-avatar" />
+    		<p>${newToy.likes} Likes </p>
+    		<button class="like-btn">Like <3</button>
+ 			</div>` 
+			e.target.reset() 
+
+		})
+	})
+
+
+
+	console.log('test');
+
+
+});
